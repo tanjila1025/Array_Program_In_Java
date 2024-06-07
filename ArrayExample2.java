6de19cd4f10d70d6936a8205be6abb1224be3f89
Tanjila Attar <158995214+tanjila1025@users.noreply.github.com>
2024-06-07 16:41:27 +0530

    Add files via upload

---
+++ ArrayExample2.java
@@ -0,0 +1,18 @@
+public class ArrayExample2 {
+    public static void main(String[] args){
+
+       int arr[]=new int[4];
+       arr[0]=2345;
+       arr[1]=2390;
+       arr[2]=2389;
+       arr[3]=2367;
+
+        for(int i=0; i<4; i++){
+            System.out.println(arr[i]);
+        }
+        //System.out.println(arr[1]);
+        //System.out.println(arr[2]);
+        //System.out.println(arr[3]);
+
+    }
+}
