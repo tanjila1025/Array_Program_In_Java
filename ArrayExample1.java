48633adce382b608dae12c28846d169dbbf5c06f
Tanjila Attar <158995214+tanjila1025@users.noreply.github.com>
2024-06-07 16:38:49 +0530

    Update ArrayExample1.java

---
+++ ArrayExample1.java
@@ -0,0 +1,23 @@
+class ArrayExample1{
+public static void main(String[] args){
+  int arr[]=new int[4];//array decleration & initailization
+  arr[0]=20;
+  arr[1]=68;
+  arr[2]=60;
+  arr[3]=34;
+
+  System.out.println(arr[0]);//for printing an array value.
+  System.out.println(arr[1]);
+  System.out.println(arr[2]);
+  System.out.println(arr[3]);
+  }
+}
+
+/*OUTPUT:
+CT:Success
+RT:Success
+  20
+  68
+  60
+  34
+  */
